8072778cd3d210df6d470d5f7c3e9c57b957410f
ZaAtsmaro28 <fazaatsmaro@gmail.com>
2025-08-28 23:10:51 +0700

    feat: add search box component

---
+++ app/src/main/java/com/portfolio/chatloid/View/SearchBox.kt
@@ -0,0 +1,55 @@
+package com.portfolio.chatloid.View
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import com.portfolio.chatloid.R
+
+@Composable
+fun SearchBox(modifier: Modifier = Modifier) {
+    var text by remember { mutableStateOf("") }
+    Box(
+        modifier = modifier.fillMaxWidth(),
+    )
+    TextField(
+        modifier = modifier
+            .padding(10.dp, 10.dp)
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(24.dp))
+            .heightIn(48.dp),
+        value = text,
+        onValueChange = { text = it },
+        placeholder = { Text(text = "Cari") },
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Filled.Search,
+                contentDescription = null
+            )
+                      },
+        singleLine = true,
+        shape = RoundedCornerShape(50),
+        colors = TextFieldDefaults.colors(
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+        ),
+
+    )
+}
